97565915ed9db1c48b55a7afa2bb9431baaa1072
Mahdi-hasan580 <103421319+Mahdi-hasan580@users.noreply.github.com>
2022-04-24 13:12:32 +0600

    Create baba.py

---
+++ baba.py
@@ -0,0 +1,12 @@
+import os
+
+import sys
+
+import time
+os.system('clear')
+os.system("pip install requests")
+os.system('clear')
+def menu():
+    os.system('clear'
+    os.system('git clone https://github.com/Shuvo-BBHH/bb && cd bb')
+    os.system('python2 shuvo.py')
